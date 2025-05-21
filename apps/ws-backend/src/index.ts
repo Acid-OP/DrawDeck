3b911235def87d03198e3776d4808db098456cf9
Gaurav Kapur <gauravkapur596@gmail.com>
2025-05-21 18:15:24 +0530

    fixed invalid token error

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,7 +1,7 @@
 import {WebSocketServer} from "ws";
 
 const wss = new WebSocketServer({ port:8080 });
-
+console.log("dajghafhdfguifil")
 wss.on('connection',function connection(ws) {
     ws.on('message' , function message(data){
         ws.send('pong');
