09726cd11f577b2c80725f643a4f9ecebdf45f38
Gaurav Kapur <gauravkapur596@gmail.com>
2025-05-25 03:06:26 +0530

    signin end point done

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,30 +1,33 @@
 import  express  from "express";
 import { middelware } from "./middelware";
-import {CreateUserSchema} from "@repo/common/types";
+import {CreateUserSchema, SigninSchema} from "@repo/common/types";
 import {prismaClient} from "@repo/db/client";
+import { JWT_SECRET } from "@repo/backend-common";
 const app = express();
 app.use(express.json());
 
 
-app.post("/api/v1/signup" , async (req,res)=> {
+app.post("/signup" , async (req,res)=> {
 
     const parsedData = CreateUserSchema.safeParse(req.body);
     if(!parsedData.success) {
+        console.log(parsedData.error)
         res.json({
             message : "Incorrect inputs"
         })
         return;
     }
     try{
-        prismaClient.user.create({
+        const user = await prismaClient.user.create({
         data: {
-            email : parsedData.data.username,
+            email : parsedData.data?.username,
+            // TODO hash the pass
             password: parsedData.data.password,
             name : parsedData.data.name
         }
     })
     res.json({
-        userId : "123"
+        userId : user.id
     }) 
     }catch(e){
         res.status(411).json({
@@ -34,8 +37,32 @@ app.post("/api/v1/signup" , async (req,res)=> {
   
 })
 
-app.post("api/v1/signin" , (req,res)=>{
-    
+app.post("api/v1/signin" , async (req,res)=>{
+    const parsedData = SigninSchema.safeParse(req.body);
+    if(!parsedData.success){
+        res.json({
+            message:"Incorrect inputs"
+        })
+        return;
+    }
+
+    // Todo : compare the password with the hased pass
+    const user = await prismaClient.user.findFirst({
+        where: {
+            email : parsedData.data.username,
+            password : parsedData.data.password
+        }
+    })
+
+    if(!user){
+        res.status(403).json({
+            message : "Not authorized"
+        })
+        return;
+    }
+    const token = jwt.sign({
+        userId : user.id
+    }, JWT_SECRET)
 })
 
 app.post("/api/v1/room" , (req ,res) => {
