3827a94b53b0a71de62459ade42daa99bff0730f
Gaurav Kapur <gauravkapur596@gmail.com>
2025-05-27 06:24:17 +0530

    added exisiting shapes logic for rectangle  on canvas

--- apps/excalidraw-front-end/app/canvas/[roomId]/page.tsx
+++ apps/excalidraw-front-end/app/canvas/[roomId]/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { initDraw } from "@/app/draw/page";
 import { useEffect } from "react";
 import { useRef } from "react"
 
@@ -8,47 +9,12 @@ export default function Canvas(){
 
     useEffect(()=> {
         if(canvasRef.current){
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext("2d");
-            
-            if(!ctx){
-                return;
-            }
-
-            let clicked = false;
-            let StartX = 0;
-            let StartY = 0;
-
-            canvas.addEventListener("mousedown" , (e)=> {
-                clicked = true
-                StartX = e.clientX
-                StartY = e.clientY
-
-            })
-
-                canvas.addEventListener("mouseup" , (e)=> {
-                clicked = false
-                console.log(e.clientX)
-                console.log(e.clientY)
-
-            })
-
-                canvas.addEventListener("mousemove" , (e)=> {
-                if(clicked){
-                    const width = e.clientX - StartX;
-                    const height = e.clientY - StartY;
-
-                    ctx.clearRect(0,0,canvas.width,canvas.height);
-                    ctx.strokeRect(StartX , StartY, width , height);
-
-                }
-
-            })
+            initDraw(canvasRef.current);
         }
 
     }, [canvasRef]);
 
     return <div>
-        <canvas ref={canvasRef} width={500} height={500}></canvas>
+        <canvas ref={canvasRef} width={2000} height={1000}></canvas>
     </div>
 }
